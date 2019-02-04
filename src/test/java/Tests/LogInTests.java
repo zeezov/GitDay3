4a817a8d60bf69017f3bef36aeeb8be5c8bdbb25
Nazim Shaazizov <nshaazizov@yahoo.com>
2019-02-03 20:23:16 -0800

    Frist commit, Login Test1

---
+++ src/test/java/Tests/LogInTests.java
@@ -0,0 +1,40 @@
+package Tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LogInTests {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void LogInTest1() {
+
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+        Assert.assertEquals(driver.getTitle(), "Web Orders");
+
+
+
+
+    }
+
+}
